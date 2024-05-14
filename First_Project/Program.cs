91682d684940c14583f85cca108d00fea4d4dca7
Serhii Zakutko <serhii.zakutko@nixsolutions.com>
2024-05-14 22:01:31 +0200

    Added homework on lesson 2

--- First_Project/Program.cs
+++ First_Project/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Привіт, я навчаюся у школі Hillel!");
+Console.ReadKey();
--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        var discountValue = CalculateDiscountValue(1500.23, 25);
+        var totalSeconds = CalculateTotalSeconds(3, 25, 10);
+        var bookInfomation = GenerateBookInformation("Кайдашева сім'я", "Іван Нечуй-Левицький", 1967, 150.99);
+
+        Console.WriteLine($"Обчислена величина знижки: {discountValue}.");
+        Console.WriteLine($"Обчислена час в секундах: {totalSeconds}с.");
+        Console.WriteLine("Інформація про книгу");
+        Console.WriteLine(bookInfomation);
+        Console.ReadKey();
+    }
+
+    public static double CalculateDiscountValue(double price, double discountPercent)
+    {
+        return price * (discountPercent / 100);
+    }
+
+    public static int CalculateTotalSeconds(int hours, int minutes, int seconds)
+    {
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public static string GenerateBookInformation(string title, string author, int year, double price)
+    {
+        return String.Format("Назва: {0}\nАвтор: {1}\nРік: {2}\nЦіна: {3:C}", title, author, year, price);
+    }
+}
