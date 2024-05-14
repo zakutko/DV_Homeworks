a05aec4f6ab510f61dd4aeba3ab1e650a776f7f2
Serhii Zakutko <serhii.zakutko@nixsolutions.com>
2024-05-14 22:25:15 +0200

    Added homework on lesson 3

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,30 +1,33 @@
-﻿public class Program
+﻿namespace Lesson_2
 {
-    public static void Main()
+    public class Program
     {
-        var discountValue = CalculateDiscountValue(1500.23, 25);
-        var totalSeconds = CalculateTotalSeconds(3, 25, 10);
-        var bookInfomation = GenerateBookInformation("Кайдашева сім'я", "Іван Нечуй-Левицький", 1967, 150.99);
+        public static void Main()
+        {
+            var discountValue = CalculateDiscountValue(1500.23, 25);
+            var totalSeconds = CalculateTotalSeconds(3, 25, 10);
+            var bookInfomation = GenerateBookInformation("Кайдашева сім'я", "Іван Нечуй-Левицький", 1967, 150.99);
 
-        Console.WriteLine($"Обчислена величина знижки: {discountValue}.");
-        Console.WriteLine($"Обчислена час в секундах: {totalSeconds}с.");
-        Console.WriteLine("Інформація про книгу");
-        Console.WriteLine(bookInfomation);
-        Console.ReadKey();
-    }
+            Console.WriteLine($"Обчислена величина знижки: {discountValue}.");
+            Console.WriteLine($"Обчислена час в секундах: {totalSeconds}с.");
+            Console.WriteLine("Інформація про книгу");
+            Console.WriteLine(bookInfomation);
+            Console.ReadKey();
+        }
 
-    public static double CalculateDiscountValue(double price, double discountPercent)
-    {
-        return price * (discountPercent / 100);
-    }
+        public static double CalculateDiscountValue(double price, double discountPercent)
+        {
+            return price * (discountPercent / 100);
+        }
 
-    public static int CalculateTotalSeconds(int hours, int minutes, int seconds)
-    {
-        return hours * 3600 + minutes * 60 + seconds;
-    }
+        public static int CalculateTotalSeconds(int hours, int minutes, int seconds)
+        {
+            return hours * 3600 + minutes * 60 + seconds;
+        }
 
-    public static string GenerateBookInformation(string title, string author, int year, double price)
-    {
-        return String.Format("Назва: {0}\nАвтор: {1}\nРік: {2}\nЦіна: {3:C}", title, author, year, price);
+        public static string GenerateBookInformation(string title, string author, int year, double price)
+        {
+            return string.Format("Назва: {0}\nАвтор: {1}\nРік: {2}\nЦіна: {3:C}", title, author, year, price);
+        }
     }
 }
---
+++ Lesson_3/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Lesson_3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Число, отримане під час прочитання числа 456 справа наліво = {ReverseNumber(456)}");
+            Console.WriteLine($"Квадрат числа 21.23 = {CalculateSquare(21.23)}");
+            Console.WriteLine($"Отримане під час перестановки першої та другої цифр числа 456 = {SwapFirstAndSecondDigit(456)}");
+            Console.WriteLine($"Середнє арифметичне чисел 34.12 та 76.2 = {CalculateAverage(34.12, 76.2)}");
+            Console.WriteLine($"Площа круга з радіусом 167.2 дорівнює {CalculateCircleArea(167.2)}");
+            Console.WriteLine("Дослідження методів заміни місцями значення двох змінних без використання тимчасової змінної:");
+            int a = 5;
+            int b = 1;
+            Console.WriteLine($"До обміну: a = {a}, b = {b}");
+            a ^= b;
+            b ^= a;
+            a ^= b;
+            Console.WriteLine($"Після обміну: a = {a}, b = {b}");
+            Console.ReadKey();
+        }
+
+        public static int ReverseNumber(int number)
+        {
+            int reversedNumber = 0;
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+                reversedNumber = reversedNumber * 10 + lastDigit;
+                number /= 10;
+            }
+            return reversedNumber;
+        }
+
+        public static double CalculateSquare(double number)
+        {
+            return Math.Pow(number, 2);
+        }
+
+        public static int SwapFirstAndSecondDigit(int number)
+        {
+            int firstDigit = number / 100;
+            int secondDigit = (number / 10) % 10;
+            int thirdDigit = number % 10;
+
+            int swappedNumber = secondDigit * 100 + firstDigit * 10 + thirdDigit;
+            return swappedNumber;
+        }
+
+        public static double CalculateAverage(double number1, double number2)
+        {
+            return (number1 + number2) / 2.0;
+        }
+
+        public static double CalculateCircleArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
