3c6d08d1a4a53980f7348129124b8388dc35f427
Serhii Zakutko <serhii.zakutko@nixsolutions.com>
2024-05-18 11:52:38 +0200

    Added homework on lesson 9

---
+++ Lesson_9/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Lesson_9.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseYear { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public Book(string title, string author, DateTime releaseYear, int numberOfPages)
+        {
+            Title = title;
+            Author = author;
+            ReleaseYear = releaseYear;
+            NumberOfPages = numberOfPages;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine($"Назва книги: {Title}");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Рік видання: {ReleaseYear.Year}");
+            Console.WriteLine($"Кількість сторінок: {NumberOfPages}");
+            Console.WriteLine("***********************");
+        }
+
+        public string IsThick()
+        {
+            return NumberOfPages > 500 ?$"Ця книга товста! Цілих {NumberOfPages} сторінок!" : $"Ця книга не твоста. Усього {NumberOfPages} сторінок.";
+        }
+    }
+}
---
+++ Lesson_9/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lesson_9.Models;
+
+namespace Lesson_9
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var book1 = new Book("Книга1", "Автор1", DateTime.Now, 565);
+            book1.DisplayInfo();
+            Console.WriteLine(book1.IsThick());
+
+            var book2 = new Book("Книга2", "Автор2", DateTime.Now, 123);
+            book2.DisplayInfo();
+            Console.WriteLine(book2.IsThick());
+
+            Console.ReadKey();
+        }
+    }
+}
